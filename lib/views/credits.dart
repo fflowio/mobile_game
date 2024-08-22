53a23c2bc14f2252886ac270e10480f95b5db701
Sarah Eggleston <segglestn@gmail.com>
2024-08-22 09:36:06 +0100

    Credits and more images, more to come

--- lib/views/credits.dart
+++ lib/views/credits.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../components/widgets.dart';
+
 class Credits {
-  static Container contents() {
-    return Container(
-      child:
-        const Text("TODO Attributions go here")
+  static Column contents() {
+    debugPrint( "https://storyset.com/sport");
+
+    return Column(
+      children: [
+        Widgets.linkWithText("Many thanks to StorySet for the illustrations", "https://storyset.com"),
+        Widgets.linkWithText("Sport illustrations", "https://storyset.com/sport"),
+        Widgets.linkWithText("Together illustrations", "https://storyset.com/together"),
+        Widgets.linkWithText("Education illustrations", "https://storyset.com/education"),
+        Widgets.linkWithText("People illustrations", "https://storyset.com/people")
+      ]
     );
   }
 }
