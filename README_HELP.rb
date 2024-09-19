459276c65681e169f1ab540d3e1b9a67f27024d2
Sarah Eggleston <segglestn@gmail.com>
2024-09-19 14:58:08 +0100

    Help README

---
+++ README_HELP.rb
@@ -0,0 +1,112 @@
+# Help
+
+A workshop for beginners, where you get to build your own flutter game.
+
+We need your help! Please try it out and tell us what you think.
+
+Here is more information about things you can help with.
+
+## Designs
+
+The design work so far has been basic, and mostly done by a developer. Please suggest improvements
+that will make this workshop more attractive and more fun!
+
+### Images
+
+The images were chosen from [storyset](https://storyset.com) because:
+
+- they are free for commercial use
+- they are multi-cultural
+
+### Colours
+
+The colours were chosen because:
+
+- they work in dark mode
+
+Everything is up for redesign: colours, images, layout.A
+
+The code is slightly easier if you use standard flutter colours from
+[this list](https://api.flutter.dev/flutter/material/Colors-class.html)
+
+But, we can use hex colours if there's a good reason to.
+
+### Layout
+
+The layout was chosen somewhat randomly.
+
+## Main instructions
+
+### Mistakes
+
+Please let us know about any mistakes in the instructions!
+
+Also, due to constraints in Markdown, they are numbered by hand, so there's definitely potential for
+numbering mistakes. Sorry!
+
+### TODOs
+
+We're working on the TODOs, if you see one and know the answer, please let us know!
+
+### Context improvements
+
+What do you think of the context explanations? Are they too long? Too short? In the wrong place?
+
+### Age-appropriate
+
+Is everything age-appropriate? A primary target for the workshop is teenagers, especially age 12-16.
+Is there anything that needs to change with this in mind?
+
+### Experience appropriate
+
+This workshop is aimed at complete beginners. We're expecting some teenagers may have experience
+with [scratch](https://scratch.mit.edu), but we don't assume it.
+
+You're still welcome if you know more about coding in general, or about flutter specifically, but
+you might find it slightly slow-paced. We'd still love your feedback!
+
+Given that it's for beginners, would you change the pace or some of the explanations?
+
+### How long did it take you?
+
+Drop a comment in the discussion area that says:
+
+- age category (under 12, 12 - 16, over 16)
+- coding experience (none, a little, passion project hacker, professional dev)
+- did you do the stretch goal? (no: ends at section 10, yes: ends at section 11)
+- how long did the workshop take you
+
+We've opened [polls](https://github.com/fflowio/mobile_game/discussions/categories/polls) for
+these in the discussion section, to make it quick to respond!
+
+## Setup
+
+We plan to run this workshop in person, where the students will have machines already set up with
+VSCode and/or Android Studio. That should be fine (or let us know if you think not...)
+
+BUT, we'd also like people to be able to grab this workshop and take it themselves for self-study.
+The workshop part might be ok, but the setup is definitely not beginner-friendly.
+
+At the moment, self-study users are going to get stuck on
+[these instructions](https://github.com/fflowio/mobile_game/blob/main/README_SETUP.md)
+
+Do you have any instructions that you think are beginner-friendly? Especially for windows?
+
+## How to send your feedback
+
+### Email
+
+Email to info@fflow.io
+
+### Github
+
+Add your thoughts to any
+[discussion](https://github.com/fflowio/mobile_game/discussions/categories/ideas)
+
+### PR
+
+Familiar with github, and have a specific fix ready to go? Submit a PR, we'll take a look
+
+
+
+
