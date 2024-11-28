40266af8234d062c4b093dc5c49203a2a45259f0
Sarah Eggleston <segglestn@gmail.com>
2024-11-28 11:11:06 +0000

    Update solution to match most recent instructions

--- lib/views/solution.dart
+++ lib/views/solution.dart
@@ -21,50 +21,15 @@ class _GameState extends State<Game> {
   List<List<String>> _sets = [];
   int _fullSets = 0;
   int _coins = 120;
-
-  Widget welcome() {
-    return Column(
-        children: [
-          const SizedBox(height: 50),
-          ImageHelpers.getPicture("assets/images/StoryboardAmico.svg", 300),
-          const SizedBox(height: 50),
-          Widgets.defaultButton(resetGame, "Start")
-        ]
-    );
-  }
-
-  void resetGame() async {
-    debugPrint("Reset");
-
-    _showContents = "start";
-    _coins = 120;
-    _fullSets = 0;
-    _set1 = [];
-    _set2 = [];
-    _set3 = [];
-    _sets = [_set1, _set2, _set3];
-
-    setState(() {  });
-  }
-
-  Widget pageContents() {
-    if (_showContents == "start") {
-      return gamePage();
-    } else if (_showContents == "finish") {
-      return finishPage();
-    } else {
-      return welcome();
-    }
-  }
-
+  
   Widget finishPage() {
     String result = "You completed $_fullSets sets!";
 
     List<Widget> children = [
       const SizedBox(height: 40),
       Text(
-          "Congratulations!!",
-          style: Widgets.celebrateTextStyle
+        "Congratulations!!",
+        style: Widgets.celebrateTextStyle
       ),
       Text(result, style: Widgets.subTextStyle),
       const SizedBox(height: 40),
@@ -75,33 +40,52 @@ class _GameState extends State<Game> {
     children += (Credits.contents());
 
     return Column(
-        children: children
+      children: children
     );
   }
 
-  Column wallet() {
-    List<Widget> contents = [
-      Widgets.setContainer(_set1),
-      const SizedBox(height: 8),
-      Widgets.setContainer(_set2),
-      const SizedBox(height: 8),
-      Widgets.setContainer(_set3)
-    ];
+  Widget pageContents() {
+    if (_showContents == "game") {
+      return gamePage();
+    } else if (_showContents == "finish") {
+      return finishPage();
+    } else {
+      return welcome();
+    }
+  }
 
+  Column cardsToChoose() {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: contents
+      children: [
+        ImageHelpers.randomPictureLink(selectCard),
+        const SizedBox(height: 10),
+        ImageHelpers.randomPictureLink(selectCard),
+        const SizedBox(height: 10),
+        ImageHelpers.randomPictureLink(selectCard)
+      ]
     );
   }
 
-  Row gamePage() {
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          cardsToChoose(),
-          wallet()
-        ]
-    );
+  addCardToNextAvailableSet(String cardName) {
+    for (var set in _sets) {
+      if (cardCanGoInSet(cardName, set)) {
+        set.add(cardName);
+        if (set.length == 4) {
+          _fullSets += 1;
+        }
+
+        return;
+      }
+    }
+  }
+
+  bool typeMatch(setCard, newCard) {
+    return setCard.substring(21, 24) == newCard.substring(21, 24);
+  }
+
+  bool categoryMatch(existingCard, newCard) {
+    debugPrint(existingCard.substring(14, 21));
+    return existingCard.substring(14, 21) == newCard.substring(14, 21);
   }
 
   bool cardCanGoInSet(String cardName, List<String> set) {
@@ -129,68 +113,86 @@ class _GameState extends State<Game> {
   }
 
   selectCard(String cardName) {
-    //debugPrint("Select card $cardName");
     addCardToNextAvailableSet(cardName);
-    _coins -= 10;
 
+    _coins -= 10;
     if (_coins == 0) {
       _showContents = "finish";
     }
 
-    setState(() {  });
+    setState(() { });
+    debugPrint("Select card $cardName");
   }
 
-  addCardToNextAvailableSet(String cardName) {
-    //debugPrint(cardName);
-    for (var set in _sets) {
-      if (cardCanGoInSet(cardName, set)) {
-        set.add(cardName);
-        if (set.length == 4) {
-          _fullSets += 1;
-        }
-        return;
-      }
-    }
-  }
+  Column wallet() {
+    List<Widget> contents = [
+      Widgets.setContainer(_set1),
+      const SizedBox(height: 8),
+      Widgets.setContainer(_set2),
+      const SizedBox(height: 8),
+      Widgets.setContainer(_set3)
+    ];
 
-  bool categoryMatch(existingCard, newCard) {
-    return existingCard.substring(14, 21) == newCard.substring(14, 21);
-  }
-
-  bool typeMatch(setCard, newCard) {
-    return setCard.substring(21, 24) == newCard.substring(21, 24);
-  }
-
-  Column cardsToChoose() {
     return Column(
-        children: [
-          ImageHelpers.randomPictureLink(selectCard),
-          const SizedBox(height: 10),
-          ImageHelpers.randomPictureLink(selectCard),
-          const SizedBox(height: 10),
-          ImageHelpers.randomPictureLink(selectCard)
-        ]
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: contents
+    );
+  }
+
+  Row gamePage() {
+    debugPrint("Game");
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        cardsToChoose(),
+        wallet()
+      ]
+    );
+  }
+
+  void resetGame() async {
+    _showContents = "game";
+    _set1 = [];
+    _set2 = [];
+    _set3 = [];
+    _sets = [_set1, _set2, _set3];
+    _fullSets = 0;
+    _coins = 120;
+    setState(() {  });
+    debugPrint("Reset");
+  }
+
+  Column welcome() {
+    return Column(
+      children: [
+        const SizedBox(height: 50),
+        ImageHelpers.getPicture("assets/images/StoryboardAmico.svg", 300),
+        const SizedBox(height: 50),
+        Widgets.defaultButton(resetGame, "Start")
+      ]
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: SizedBox(
-                width: 375,
-                child: Column(
-                    children: [
-                      Widgets.customAppBar(Game.name, _coins),
-                      Container(
-                          color: Colors.deepPurple,
-                          constraints: const BoxConstraints(maxHeight: 580),
-                          child: pageContents()
-                      )
-                    ]
-                )
-            )
+      body: Center(
+        child: SizedBox(
+          width: 375,
+          child: Column(
+            children: [
+              Widgets.customAppBar(Game.name, _coins),
+              Container(
+                color: Colors.deepPurple,
+                width: double.infinity,
+                constraints: const BoxConstraints(maxHeight: 580),
+                child: pageContents()
+              )
+            ]
+          )
         )
+      )
     );
   }
 }
